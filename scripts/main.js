57a4e98a3d288fce68360dc33383b9734e784947
gausoft <thiergau691@gmail.com>
2020-01-27 02:42:43 +0000

    feat: Minimum disign oki

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
 
 $(document).ready(function() {
+
+  $('#gausoftage').html((new Date().getFullYear()) - 1996)
+
   AOS.init( {
     // uncomment below for on-scroll animations to played only once
     // once: true  
